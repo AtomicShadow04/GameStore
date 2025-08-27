807a888608247bab9758a27de5d675682f6d1724
funbimd <funbi.mdabubakar@yahoo.com>
2025-08-27 09:50:17 +0100

    Update navigation behavior in DeleteGame and EditGame components to force reload the home page after operations

--- GameStore.Frontend/obj/Debug/net9.0/GameStore.Frontend.AssemblyInfo.cs
+++ GameStore.Frontend/obj/Debug/net9.0/GameStore.Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6e470046d714e89a29f0936dd77f17a1c3a607a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74aa038ac6b9b6717ebe80fe39afaa1b4b5883eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
